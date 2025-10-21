5e04a1e4db56e4ee770400b0311e459e86c4247a
Hieuzest <girkirin@hotmail.com>
2025-10-21 18:24:23 +0800

    feat: support export api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { } from '@hieuzest/koishi-plugin-mahjong'
+import { } from '@koishijs/plugin-server'
 import { Context, Dict, HTTP, Schema, Session } from 'koishi'
 import * as OB from './ob'
 import { decodeAccountId, getAccountZone, judgeLevel, levelMax, levelStart } from './utils'
@@ -26,6 +27,7 @@ export class Mspt {
     ctx.command('mspt [pattern:rawtext]')
       .option('sapk', '-f')
       .option('server', '-s')
+      .option('type', '-t <type:string>')
       .option('bind', '-b')
       .usage('pattern: NICKNAME / $AID / $$EID')
       .userFields(['mspt/bind'])
@@ -38,7 +40,12 @@ export class Mspt {
         }
         let ret: Dict<Mspt.Result> = null
         if (pattern[0] === '$') ret = await this.processQuery({}, parseInt(pattern.slice(1)), null)
-        else { ret = await this.processQuery({}, null, pattern, { rankQueryingPreference: options.server ? 'server' : options.sapk ? 'sapk' : undefined }) }
+        else {
+          ret = await this.processQuery({}, null, pattern, {
+            rankQueryingPreference: options.server ? 'server' : options.sapk ? 'sapk' : undefined,
+            matchType: options.type as any,
+          })
+        }
         if (ret && Object.keys(ret).length) return Object.values(ret).map(r => this.generateReply(session, r)).join('\n')
         else return session.text('.failed')
       })
@@ -70,49 +77,85 @@ export class Mspt {
           nickname: res.nickname,
           m4: Mspt.generateDescription(res, 'level'),
           m3: Mspt.generateDescription(res, 'level3'),
+          raw4: res['level'],
+          raw3: res['level3'],
           src: 'server',
         }
 
         return this.generateReply(session, result)
       })
+
+    if (this.config.exportApi) {
+      ctx.inject(['server'], (ctx) => {
+        ctx.server.get(this.config.exportApiEndpoint, async (kCtx) => {
+          const { pattern, aid, rank, type } = kCtx.request.query as Dict<string>
+          const options: Mspt.Preference = {
+            aidQueryingPreference: aid as Mspt.QueryingPreference,
+            rankQueryingPreference: rank as Mspt.QueryingPreference,
+            matchType: type as any,
+          }
+          let ret: Dict<Mspt.Result> = null
+
+          if (pattern.startsWith('$$')) {
+            const accountId = decodeAccountId(parseInt(pattern.slice(2)))
+            ret = await this.processQuery({}, accountId, null, options)
+          } else if (pattern.startsWith('$')) {
+            const accountId = parseInt(pattern.slice(1))
+            ret = await this.processQuery({}, accountId, null, options)
+          } else {
+            ret = await this.processQuery({}, null, pattern, options)
+          }
+
+          kCtx.type = 'json'
+          kCtx.body = ret
+        })
+      })
+    }
   }
 
-  async queryAidFromSapk(res: Dict<Mspt.Result>, nickname: string) {
+  async queryAidFromSapk(res: Dict<Mspt.Result>, nickname: string, options: Mspt.Preference) {
     const quotename = encodeURIComponent(nickname)
-    try {
-      const data = await this.http.get(`${this.config.sapkUri}/search_player/${quotename}`, {
-        params: { limit: 9 },
-      })
-      for (const d of data || []) {
-        if (d.nickname.trim() === nickname) {
-          res[d.id] = res[d.id] || {
-            accountId: d.id,
-            nickname: d.nickname,
+
+    if (options.matchType !== '3') {
+      try {
+        const data = await this.http.get(`${this.config.sapkUri}/search_player/${quotename}`, {
+          params: { limit: 9 },
+        })
+        for (const d of data || []) {
+          if (d.nickname.trim() === nickname) {
+            res[d.id] = res[d.id] || {
+              accountId: d.id,
+              nickname: d.nickname,
+            }
+            res[d.id].m4 = Mspt.generateDescription(d)
+            res[d.id].raw4 = d['level']
+            res[d.id].src = 'sapk'
           }
-          res[d.id].m4 = Mspt.generateDescription(d)
-          res[d.id].src = 'sapk'
         }
+      } catch (e) {
+        this.ctx.logger.debug(`Fail to query ${nickname} from sapk`)
       }
-    } catch (e) {
-      this.ctx.logger.debug(`Fail to query ${nickname} from sapk`)
     }
 
-    try {
-      const data = await this.http.get(`${this.config.sapkTriUri}/search_player/${quotename}`, {
-        params: { limit: 9 },
-      })
-      for (const d of data || []) {
-        if (d.nickname.trim() === nickname) {
-          res[d.id] = res[d.id] || {
-            accountId: d.id,
-            nickname: d.nickname,
+    if (options.matchType !== '4') {
+      try {
+        const data = await this.http.get(`${this.config.sapkTriUri}/search_player/${quotename}`, {
+          params: { limit: 9 },
+        })
+        for (const d of data || []) {
+          if (d.nickname.trim() === nickname) {
+            res[d.id] = res[d.id] || {
+              accountId: d.id,
+              nickname: d.nickname,
+            }
+            res[d.id].m3 = Mspt.generateDescription(d)
+            res[d.id].raw3 = d['level']
+            res[d.id].src = 'sapk'
           }
-          res[d.id].m3 = Mspt.generateDescription(d)
-          res[d.id].src = 'sapk'
         }
+      } catch (e) {
+        this.ctx.logger.debug(`Fail to query ${nickname} from sapk`)
       }
-    } catch (e) {
-      this.ctx.logger.debug(`Fail to query ${nickname} from sapk`)
     }
 
     // Update account_map
@@ -124,40 +167,47 @@ export class Mspt {
     return Object.values(res).map(v => v.accountId)
   }
 
-  async queryRankFromSapk(res: Dict<Mspt.Result>, accountId: number, forceUpdate: boolean = false) {
+  async queryRankFromSapk(res: Dict<Mspt.Result>, accountId: number, forceUpdate: boolean, options: Mspt.Preference) {
     if (accountId in res && !forceUpdate) return true
-    try {
-      const d = await this.http.get(`${this.config.sapkUri}/player_stats/${accountId}/1262304000000/${Date.now()}`, {
-        params: { mode: '16.12.9.15.11.8' },
-      })
-      if (d && !('error' in d)) {
-        res[d.id] = res[d.id] || {
-          accountId: d.id,
-          nickname: d.nickname,
+
+    if (options.matchType !== '3') {
+      try {
+        const d = await this.http.get(`${this.config.sapkUri}/player_stats/${accountId}/1262304000000/${Date.now()}`, {
+          params: { mode: '16.12.9.15.11.8' },
+        })
+        if (d && !('error' in d)) {
+          res[d.id] = res[d.id] || {
+            accountId: d.id,
+            nickname: d.nickname,
+          }
+          res[d.id].m4 = Mspt.generateDescription(d)
+          res[d.id].raw4 = d['level']
+          if ('max_level' in d) res[d.id].hm4 = Mspt.generateDescription(d, 'max_level')
+          res[d.id].src = 'sapk'
         }
-        res[d.id].m4 = Mspt.generateDescription(d)
-        if ('max_level' in d) res[d.id].hm4 = Mspt.generateDescription(d, 'max_level')
-        res[d.id].src = 'sapk'
+      } catch (e) {
+        this.ctx.logger.debug(`Fail to query $${accountId} from sapk`, e)
       }
-    } catch (e) {
-      this.ctx.logger.debug(`Fail to query $${accountId} from sapk`, e)
     }
 
-    try {
-      const d = await this.http.get(`${this.config.sapkTriUri}/player_stats/${accountId}/1262304000000/${Date.now()}`, {
-        params: { mode: '26.24.22.25.23.21' },
-      })
-      if (d && !('error' in d)) {
-        res[d.id] = res[d.id] || {
-          accountId: d.id,
-          nickname: d.nickname,
+    if (options.matchType !== '4') {
+      try {
+        const d = await this.http.get(`${this.config.sapkTriUri}/player_stats/${accountId}/1262304000000/${Date.now()}`, {
+          params: { mode: '26.24.22.25.23.21' },
+        })
+        if (d && !('error' in d)) {
+          res[d.id] = res[d.id] || {
+            accountId: d.id,
+            nickname: d.nickname,
+          }
+          res[d.id].m3 = Mspt.generateDescription(d)
+          res[d.id].raw3 = d['level']
+          if ('max_level' in d) res[d.id].hm3 = Mspt.generateDescription(d, 'max_level')
+          res[d.id].src = 'sapk'
         }
-        res[d.id].m3 = Mspt.generateDescription(d)
-        if ('max_level' in d) res[d.id].hm3 = Mspt.generateDescription(d, 'max_level')
-        res[d.id].src = 'sapk'
+      } catch (e) {
+        this.ctx.logger.debug(`Fail to query $${accountId} from sapk`)
       }
-    } catch (e) {
-      this.ctx.logger.debug(`Fail to query $${accountId} from sapk`)
     }
 
     return true
@@ -192,12 +242,14 @@ export class Mspt {
             nickname: ret.nickname,
             m4: Mspt.generateDescription(ret, 'level'),
             m3: Mspt.generateDescription(ret, 'level3'),
+            raw4: ret['level'],
+            raw3: ret['level3'],
             src: 'server',
           }
           return res
         }
       }
-      await this.queryRankFromSapk(res, accountId)
+      await this.queryRankFromSapk(res, accountId, false, options)
       return res
     } else if (nickname) {
       let aids = []
@@ -206,7 +258,7 @@ export class Mspt {
         this.ctx.logger.debug(`query ${nickname}: Query aids from OB: ${aids}`)
       }
       if (!aids.length) {
-        aids = [...new Set([...aids, ...await this.queryAidFromSapk(res, nickname)])]
+        aids = [...new Set([...aids, ...await this.queryAidFromSapk(res, nickname, options)])]
         this.ctx.logger.debug(`query ${nickname}: Query aids from sapk: ${aids}`)
       }
       for (const aid of aids) await this.processQuery(res, aid, undefined, options)
@@ -226,7 +278,7 @@ export class Mspt {
 export namespace Mspt {
   export const inject = {
     required: ['database'],
-    optional: ['mahjong', 'mahjong.majsoul', 'mahjong.database'],
+    optional: ['mahjong', 'mahjong.majsoul', 'mahjong.database', 'server'],
   }
 
   export interface Result {
@@ -237,6 +289,8 @@ export namespace Mspt {
     hm4?: string
     hm3?: string
     src?: 'failed' | 'failed-server' | 'server' | 'sapk' | 'sync' | 'subscription' | 'playing'
+    raw4?: any
+    raw3?: any
   }
 
   export function generateDescription(data, label: string = 'level') {
@@ -263,15 +317,18 @@ export namespace Mspt {
     return msg
   }
 
-  type QueryingPreference = 'database' | 'sapk' | 'server'
+  export type QueryingPreference = 'database' | 'sapk' | 'server'
 
   export interface Preference {
     aidQueryingPreference?: QueryingPreference
     rankQueryingPreference?: QueryingPreference
+    matchType?: '4' | '3'
   }
   export interface Config extends Preference {
     sapkUri: string
     sapkTriUri: string
+    exportApi: boolean
+    exportApiEndpoint: string
   }
 
   export const Config: Schema<Config> = Schema.object({
@@ -279,6 +336,8 @@ export namespace Mspt {
     sapkTriUri: Schema.string().default('https://5-data.amae-koromo.com/api/v2/pl3'),
     aidQueryingPreference: Schema.union<QueryingPreference>(['database', 'sapk']).default('sapk'),
     rankQueryingPreference: Schema.union<QueryingPreference>(['database', 'sapk', 'server']).default('sapk'),
+    exportApi: Schema.boolean().default(false),
+    exportApiEndpoint: Schema.string().default('/mspt'),
   })
 }
 
--- src/ob.ts
+++ src/ob.ts
@@ -51,6 +51,14 @@ export async function queryFromObById(ctx: Context, accountId: number): Promise<
             ...player.level3,
           },
         }),
+        raw4: {
+          'delta': doc.wg.players.length === 4 ? dpt : 0,
+          ...player.level,
+        },
+        raw3: {
+          'delta': doc.wg.players.length === 3 ? dpt : 0,
+          ...player.level3,
+        },
         src,
         // hm4: '',
         // hm3: '',
